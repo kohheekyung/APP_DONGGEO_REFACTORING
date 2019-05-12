b1a508277aeff7d22d53c0bd6e045ab591d1b663
ohsoyoung <soyoung466@naver.com>
2019-05-12 17:41:57 +0900

    search~~

--- app/src/main/java/donggeo/appdonggeorefactoring/SearchByValue.java
+++ app/src/main/java/donggeo/appdonggeorefactoring/SearchByValue.java
@@ -87,7 +87,6 @@ public class SearchByValue extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-
         exchangeInput = (EditText)findViewById(R.id.search_exchangeInput);
         schoolInput =  (EditText)findViewById(R.id.search_schoolInput);
 
